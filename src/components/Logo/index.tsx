84f7a18d651938f268d63e0ffe8e0257b6a72938
nabin <nabin.pandey@ebpearls.com>
2021-11-09 22:15:16 +0545

    feat: fix logo size

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,6 @@ import Logo1 from './Logo.png';
 const LogoWrapper = experimentalStyled(Box)(
   ({ theme }) => `
        
-       
         padding:4px;
 `
 );
@@ -13,7 +12,11 @@ const LogoWrapper = experimentalStyled(Box)(
 function Logo() {
   return (
     <LogoWrapper>
-      <img src={Logo1} alt="logo" />
+      <img
+        style={{ height: '30px', width: 'auto', marginLeft: '18px' }}
+        src={Logo1}
+        alt="logo"
+      />
     </LogoWrapper>
   );
 }
